78eb3fabd894d01684783128749fc6ef8953df94
n-f9 <ndferguson@protonmail.com>
2022-06-25 14:55:06 -0400

    more border stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include <ti/getcsc.h>
 
 int main(void) {
+	const int BORDERWIDTH = 3;
+
 	gfx_Begin();
 	
-	gfx_Rectangle(2,2, 315, 236);
+	gfx_Rectangle(BORDERWIDTH, BORDERWIDTH, GFX_LCD_WIDTH-(2 * BORDERWIDTH), GFX_LCD_HEIGHT-(2 * BORDERWIDTH));
+
+	gfx_PrintStringXY("Hello, World", 2*BORDERWIDTH, 2*BORDERWIDTH);
+	gfx_PrintStringXY("Hello, World", 2*BORDERWIDTH, 2*BORDERWIDTH +(8+BORDERWIDTH));
 
   while (!os_GetCSC());
 
