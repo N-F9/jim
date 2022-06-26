a32f71901243a5d5a95e3dae65408d8d3adc2fe1
n-f9 <ndferguson@protonmail.com>
2022-06-26 08:12:01 -0400

    ui

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 void printList(char* OPTIONS[], int BORDERWIDTH);
+void render(int page, int x, int y);
 
 int main(void) {
 	const int BORDERWIDTH = 3;
@@ -14,6 +15,7 @@ int main(void) {
 		"2 Points",
 		"Synthetic /",
 	};
+	int page = 0;
 
 	gfx_Begin();
 	
@@ -23,15 +25,64 @@ int main(void) {
 	gfx_PrintStringXY("JIM", 2*BORDERWIDTH, 2*BORDERWIDTH);
 	printList(OPTIONS, BORDERWIDTH);
 
-  while (!os_GetCSC());
+	
+	render(page, GFX_LCD_WIDTH-GFX_LCD_HEIGHT+BORDERWIDTH, BORDERWIDTH);
 
-	gfx_End();
+  while (true) {
+		sk_key_t key;
+    while (key = os_GetCSC() && !os_GetCSC());
+		dbg_printf("%d\n", key);
+		gfx_SetColor(255);
+		gfx_FillRectangle(GFX_LCD_WIDTH-GFX_LCD_HEIGHT+BORDERWIDTH+1, BORDERWIDTH+1, GFX_LCD_HEIGHT-(2 * BORDERWIDTH)-2, GFX_LCD_HEIGHT-(2 * BORDERWIDTH)-2);
+		gfx_SetColor(0);
+		// gfx_FloodFill(GFX_LCD_WIDTH-GFX_LCD_HEIGHT+BORDERWIDTH+1, BORDERWIDTH+1, 0);
+		// gfx_Rectangle(BORDERWIDTH, BORDERWIDTH, GFX_LCD_WIDTH-(2 * BORDERWIDTH), GFX_LCD_HEIGHT-(2 * BORDERWIDTH));
+		// gfx_Rectangle(GFX_LCD_WIDTH-GFX_LCD_HEIGHT+BORDERWIDTH, BORDERWIDTH, GFX_LCD_HEIGHT-(2 * BORDERWIDTH), GFX_LCD_HEIGHT-(2 * BORDERWIDTH));
+		
 
-	return 0;
+		if (key == sk_Up && page > 0) {
+			--page;
+		}
+
+		if (key == sk_Down && page < 5) {
+			++page;
+		}
+
+		if (key == sk_Clear) {
+			gfx_End();
+			return 0;
+		}
+		
+		render(page, GFX_LCD_WIDTH-GFX_LCD_HEIGHT+BORDERWIDTH, BORDERWIDTH);
+
+	}
 }
 
 void printList(char* OPTIONS[], int BORDERWIDTH) {
 	for (int i = 0; i < 5; i++) { // fix
 		gfx_PrintStringXY(OPTIONS[i], 2*BORDERWIDTH, 2*BORDERWIDTH + (i+1) * (8+BORDERWIDTH));
+	}
+}
+
+void render(int page, int x, int y) {
+	switch (page) {
+		case 0:
+			gfx_PrintStringXY("tri", x, y);
+			break;
+		case 1:
+			gfx_PrintStringXY("shapes", x, y);
+			break;
+		case 2:
+			gfx_PrintStringXY("uc", x, y);
+			break;
+		case 3:
+			gfx_PrintStringXY("2p", x, y);
+			break;
+		case 4:
+			gfx_PrintStringXY("s/", x, y);
+			break;
+		
+		default:
+			break;
 	}
 }
